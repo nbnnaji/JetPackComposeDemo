6049c21611cb8a9b572dfe67ff34abdc1eb83c81
Nkechi Nnaji <nkechi_nnaji@ORFNNAJNMACX10B.local>
2022-04-29 23:32:44 -0500

    Animated composable functions added

--- app/src/main/java/com/nnaji/jetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/nnaji/jetpackcomposedemo/MainActivity.kt
@@ -1,11 +1,118 @@
 package com.nnaji.jetpackcomposedemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nnaji.samplejetpack.ui.theme.SampleJetPackTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            SampleJetPackTheme{
+                Conversation(
+                  SampleData.conversationSample
+                )
+            }
+        }
     }
 }
+
+
+data class Message(val author: String, val body: String)
+
+
+@Composable
+fun MessageCard(msg: Message) {
+
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_profile_pic_foreground),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        //Add horizontal space between image and the column
+        Spacer( modifier = Modifier.width(8.dp))
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+        Column (modifier = Modifier.clickable { isExpanded = !isExpanded }){
+            Text(text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2)
+
+            Spacer( modifier = Modifier.width(4.dp))
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+            Text(text = msg.body,
+                modifier = Modifier.padding(all = 4.dp),
+                // If the message is expanded, we display all its content
+                // otherwise we only display the first line
+                maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                style = MaterialTheme.typography.body2)
+        }
+        }
+    }
+
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+
+
+@Composable
+fun PreviewMessageCard() {
+    SampleJetPackTheme {
+        MessageCard(
+            msg = Message(
+                "Nkechi Nnaji",
+                "Getting ready for Jet Pack Compose"
+            )
+        )
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+
